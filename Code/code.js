6f5e661a34e1a36ac6662f7d54791e8bfeb0bc9d
ProyectoCecy <128453267+ProyectoCecy@users.noreply.github.com>
2023-06-06 22:54:31 -0600

    Add files via upload

--- Code/code.js
+++ Code/code.js
@@ -359,3 +359,49 @@ window.addEventListener('load', function() {
   }
 });
 
+
+
+
+/* ESTA PARTE ES DE LA LIBRERIA: https://app.embed.im/snow.js Como no se podia poner bien, se extrajo el codigo que venia del enlace */
+
+
+var embedimSnow = document.getElementById("embedim--snow");
+
+if (!embedimSnow) {
+  function embRand(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+  }
+
+  var embCSS = '.embedim-snow{position: absolute;width: 10px;height: 10px;background: white;border-radius: 50%;margin-top: -10px}';
+  var embHTML = '';
+
+  for (var i = 1; i < 200; i++) {
+    embHTML += '<i class="embedim-snow"></i>';
+
+    var rndX = (embRand(0, 1000000) * 0.0001);
+    var rndO = embRand(-100000, 100000) * 0.0001;
+    var rndT = (embRand(3, 8) * 10).toFixed(2);
+    var rndS = (embRand(0, 10000) * 0.0001).toFixed(2);
+
+    embCSS += '.embedim-snow:nth-child(' + i + '){' +
+      'opacity:' + (embRand(1, 10000) * 0.0001).toFixed(2) + ';' +
+      'transform:translate(' + rndX.toFixed(2) + 'vw,-10px) scale(' + rndS + ');' +
+      'animation:fall-' + i + ' ' + embRand(10, 30) + 's -' + embRand(0, 30) + 's linear infinite' +
+      '}' +
+      '@keyframes fall-' + i + '{' +
+      rndT + '%{' +
+      'transform:translate(' + (rndX + rndO).toFixed(2) + 'vw,' + rndT + 'vh) scale(' + rndS + ')' +
+      '}' +
+      'to{' +
+      'transform:translate(' + (rndX + (rndO / 2)).toFixed(2) + 'vw, 105vh) scale(' + rndS + ')' +
+      '}' +
+      '}';
+  }
+
+  embedimSnow = document.createElement('div');
+  embedimSnow.id = 'embedim--snow';
+  embedimSnow.innerHTML = '<style>#embedim--snow {position: fixed; left: 0; top: 0; bottom: 0; width: 100%; height: 100%; overflow: hidden; z-index: 1; pointer-events: none}' + embCSS + '</style>' + embHTML;
+
+  var msjDiv = document.querySelector(".msj");
+  msjDiv.appendChild(embedimSnow);
+}
